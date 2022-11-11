79c926fe85ae7df31be7a4a40a4c64d6691969e7
User Name <email@domain.co.za>
2022-11-11 14:06:48 +0200

    Added the new Test Project with a category controller test

---
+++ DonationUnitTest/CategoryControllerUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using LoginSec.Areas.Identity.Data;
+using LoginSec.Controllers;
+using LoginSec.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DonationUnitTest
+{
+    [TestClass]
+    public class CategoryControllerUnitTest
+    {
+        //Testing the data returned by the category controller
+        private readonly ApplicationDbContext _context;
+        [TestMethod]
+        public void ControllerTest_ReturnMonetaryString()
+        {
+            var controller = new CategoryController(_context);
+            var result = controller.AddOrEdit() as Microsoft.AspNetCore.Mvc.ViewResult;
+            var product = (Category)result.ViewData.Model;
+            Assert.AreEqual("Monetary", product.Type);
+        }
+    }
+}
